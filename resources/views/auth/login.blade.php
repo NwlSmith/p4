d984ff94615f7087bb836852cf3df62aadd7d679
Nate Smith <me@nwlsmith.com>
2019-05-15 23:52:10 -0400

    ACTUAL final commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,37 @@
 @extends('layouts.master')
 
 @section('content')
+    <div id='gamePage'>
+        <h1>Login</h1>
 
-    <h1>Login</h1>
+        Don't have an account? <a href='/register'>Register here...</a>
 
-    Don't have an account? <a href='/register'>Register here...</a>
+        <form method='POST' action='{{ route('login') }}'>
 
-    <form method='POST' action='{{ route('login') }}'>
+            {{ csrf_field() }}
 
-        {{ csrf_field() }}
+            <label for='email'>E-Mail Address</label>
+            <input id='email' type='email' name='email' value='{{ old('email') }}' required autofocus>
+            @include('includes.error-field', ['fieldName' => 'email'])
+            <br>
 
-        <label for='email'>E-Mail Address</label>
-        <input id='email' type='email' name='email' value='{{ old('email') }}' required autofocus>
-        @include('includes.error-field', ['fieldName' => 'email'])
+            <label for='password'>Password</label>
+            <input id='password' type='password' name='password' required>
+            @include('includes.error-field', ['fieldName' => 'password'])
+            <br>
 
-        <label for='password'>Password</label>
-        <input id='password' type='password' name='password' required>
-        @include('includes.error-field', ['fieldName' => 'password'])
+            <label>
+                <input type='checkbox' name='remember' {{ old('remember') ? 'checked' : '' }}> Remember Me
+            </label>
+            <br>
+            <br>
 
-        <label>
-            <input type='checkbox' name='remember' {{ old('remember') ? 'checked' : '' }}> Remember Me
-        </label>
+            <button type='submit' id='login' class='linkButton'>Login</button>
+            <br>
+            <br>
 
-        <button type='submit' class='btn btn-primary'>Login</button>
+            <a class='linkButton' href='{{ route('password.request') }}'>Forgot Your Password?</a>
 
-        <a class='btn btn-link' href='{{ route('password.request') }}'>Forgot Your Password?</a>
-
-    </form>
-
+        </form>
+    </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,28 +1,35 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Register</h1>
+    <div id='gamePage'>
+        <h1>Register</h1>
 
-    Already have an account? <a href='/login'>Login here...</a>
+        Already have an account? <a href='/login'>Login here...</a>
 
-    <form method='POST' action='{{ route('register') }}'>
-        {{ csrf_field() }}
+        <form method='POST' action='{{ route('register') }}'>
+            {{ csrf_field() }}
 
-        <label for='name'>Name</label>
-        <input id='name' type='text' name='name' value='{{ old('name') }}' required autofocus>
-        @include('includes.error-field', ['fieldName' => 'name'])
+            <label for='name'>Name</label>
+            <input id='name' type='text' name='name' value='{{ old('name') }}' required autofocus>
+            @include('includes.error-field', ['fieldName' => 'name'])
+            <br>
 
-        <label for='email'>E-Mail Address</label>
-        <input id='email' type='email' name='email' value='{{ old('email') }}' required>
-        @include('includes.error-field', ['fieldName' => 'email'])
+            <label for='email'>E-Mail Address</label>
+            <input id='email' type='email' name='email' value='{{ old('email') }}' required>
+            @include('includes.error-field', ['fieldName' => 'email'])
+            <br>
 
-        <label for='password'>Password (min: 8)</label>
-        <input id='password' type='password' name='password' required>
-        @include('includes.error-field', ['fieldName' => 'password'])
+            <label for='password'>Password (min: 8)</label>
+            <input id='password' type='password' name='password' required>
+            @include('includes.error-field', ['fieldName' => 'password'])
+            <br>
 
-        <label for='password-confirm'>Confirm Password</label>
-        <input id='password-confirm' type='password' name='password_confirmation' required>
+            <label for='password-confirm'>Confirm Password</label>
+            <input id='password-confirm' type='password' name='password_confirmation' required>
+            <br>
 
-        <button type='submit' class='btn btn-primary'>Register</button>
-    </form>
+            <button type='submit' class='linkButton'>Register</button>
+            <br>
+        </form>
+    </div>
 @endsection
